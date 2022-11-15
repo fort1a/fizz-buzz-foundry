941d0b6f6cf769cba4065561f89c235d6b80b7c6
Joon Kim <joon@fort1a.com>
2022-11-14 20:29:30 -0800

    initial commit

--- src/FizzBuzz.sol
+++ src/FizzBuzz.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
-import "openzeppelin-contracts/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract FizzBuzz {
     function fizzbuzz(uint256 x) public pure returns (string memory) {
@@ -11,7 +11,7 @@ contract FizzBuzz {
         if (x % 5 == 0) {
             return "buzz";
         }
-        if (x % 3 == 0 && n % 5 == 0) {
+        if (x % 3 == 0 && x % 5 == 0) {
             return "fizzbuzz";
         }
         return Strings.toString(x);
---
+++ test/FizzBuzz.test.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol"
+import "src/FizzBuzz.sol"
+
+contract FizzBuzzTest is Test {
+    
+}
